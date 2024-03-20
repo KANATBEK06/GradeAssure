dd039981726da23d24086c8fb46cae47277f7ac2
KANATBEK06 <kanatbekmusaev0@gmail.com>
2024-03-21 01:01:55 +0600

    'Create-Footer'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+// const App = () => {
+//   return <div />
+// }
+// export default App
+
+import React from 'react'
+import Footer from './components/UI/Footer/Footer'
+
 const App = () => {
-  return <div />
+  return (
+    <div>
+      <Footer />
+    </div>
+  )
 }
+
 export default App
